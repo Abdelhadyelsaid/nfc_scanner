50e0e55f0ab229ebed87aa78f2abe1672f027690
Abdelhady <shalsh902@gmail.com>
2022-12-02 11:36:18 +0200

    edit base url with name

--- nfc_scanner/lib/screens/nfc_scan.dart
+++ nfc_scanner/lib/screens/nfc_scan.dart
@@ -50,16 +50,17 @@ class _Nfc_scanState extends State<Nfc_scan> {
       }
       id = id.substring(3);
       List <String> data =id.split(",");
-      print("This is Id from Tag reader ! :${data[1]}");
+      print("This is Id from Tag reader ! :${data[1]},\n This is name: ${data[0]}");
+
       Response response= await http.get(
-        Uri.parse("http://192.168.1.10:3000/${data[1]}"),
+        Uri.parse("http://192.168.1.10:3000/${data[1]}/${data[0]}"),
         headers: <String, String>{
           'Content-Type': 'application/json; charset=UTF-8',
         },
       );
       if(response.statusCode == 200){
         print("Sent Reuest Successfully!");
-        
+
       }
       else{
         print("Error in request! ");
